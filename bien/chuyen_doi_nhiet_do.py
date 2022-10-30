2d9659f3b3122792c9a8bb56c7718bf711a2d41b
tuanhk <tuanhuynh191088@gmail.com>
2022-10-30 20:23:26 +0700

    update

---
+++ bien/chuyen_doi_nhiet_do.py
@@ -0,0 +1,6 @@
+import math
+c = int(input("Nhập độ C: "))
+
+f = (9*(c)/5)+32
+
+print("Độ F là: ",f)
--- turtle/.history/ve_mat_cuoi_20221017210944.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-turtle.pensize(5)
-turtle.color('blue')
-face_size = 200
-turtle.penup()
-turtle.goto(0,-200)
-turtle.pendown()
-turtle.circle(face_size)
