a62b4089a6eb4debf4c79cefd270072bfa753e75
tuanhk <tuanhuynh191088@gmail.com>
2022-12-03 00:56:59 +0700

    bmi

---
+++ bien/tinh_bmi.py
@@ -0,0 +1,24 @@
+chieuCao = input("Nhập chiều cao (m): ")
+canNang = input("Nhập cân nặng (kg): ")
+
+if chieuCao.isnumeric() == False :
+    print("Chiều cao không hợp lệ")
+elif canNang.isnumeric() == False :
+    print("Cân nặng không hợp lệ")
+if float(chieuCao) == 0 or float(canNang) == 0:
+    print("Không hợp lệ")    
+else:
+    bmi = float(canNang) / (float(chieuCao) * float(chieuCao))
+    msg = "Gầy cấp độ III"
+    if bmi >= 16 and bmi < 17 : msg = "Gầy cấp độ II"
+    if bmi >= 17 and bmi < 18.5 : msg = "Gầy cấp độ I"
+    if bmi >= 18.5 and bmi < 25 : msg = "Bình thường"
+    if bmi >= 25 and bmi < 30 : msg = "Thừa cân"
+    if bmi >= 30 and bmi < 35  : msg = "Béo phì cấp độ I"
+    if bmi >= 35 and bmi < 40 : msg = "Béo phì cấp độ II"
+    if bmi >= 40 : msg = "Béo phì cấp độ III"
+
+    print("Số bmi là {b}. Kết quả {k}".format(b=bmi,k=msg))
+    
+
+     
