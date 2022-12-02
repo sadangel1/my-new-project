f4340d9e9beb59ebab5fc2c96c80415c65440cd4
tuanhk <tuanhuynh191088@gmail.com>
2022-12-03 00:11:27 +0700

    Cap Nhat Thu

---
+++ bien/kiem_tra_thu.py
@@ -0,0 +1,18 @@
+num = input("Nhap so 1 - 7:")
+if num.isnumeric() == False or int(num) > 7 or int(num) < 0:
+    print("Số không hợp lệ")
+else:
+    num = int(num)
+    if num == 1: print("Chủ nhật")
+    elif num == 2: print("Thứ 2")
+    elif num == 3: print("Thứ 3")
+    elif num == 4: print("Thứ 4")
+    elif num == 5: print("Thứ 5")
+    elif num == 6: print("Thứ 6")
+    elif num == 7: print("Thứ 7")
+
+
+#   Hoặc 
+
+    #if num == 1: print("Chủ nhật")
+    #else: print("Thứ "+str(num))
